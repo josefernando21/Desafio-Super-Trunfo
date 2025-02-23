bbe0a701d9bb63448adfae08c8b70a954696669d
josefernando21 <njosefernando62@gmail.com>
2025-02-23 19:48:19 +0000

    tirei os caracteres especiais

--- super_trunfo.c
+++ super_trunfo.c
@@ -3,78 +3,78 @@
  int main(){
 
 char Estado1[50];
-char Códigodacarta1[50];
+char Codigodacarta1[50];
 char Nomedacidade1[50];
-int População1;
-float ÁreaemKm²1;
+int Populacao1;
+float AreaemKm1;
 float PIB1;
-int Númerosdepontosturísticos1;
+int Numerosdepontosturisticos1;
 
 char Estado2[50];
-char Códigodacarta2[50];
+char Codigodacarta2[50];
 char Nomedacidade2[50];
-int População2;
-float ÁreaemKm²2;
+int Populacao2;
+float AreaemKm2;
 float PIB2;
-int Númerosdepontosturísticos2;
+int Numerosdepontosturisticos2;
 
 printf("Digite seu estado:\n");
 scanf("%s", Estado1);
 
 printf("Digite seu Código:\n");
-scanf("%s", Códigodacarta1);
+scanf("%s", Codigodacarta1);
 
 printf("Digite sua cidade:\n");
 scanf("%s", Nomedacidade1);
 
 printf("Digite sua população:\n");
-scanf("%d", &População1);
+scanf("%d", &Populacao1);
 
 printf("Digite sua área em Km²:\n");
-scanf("%f", &ÁreaemKm²1);
+scanf("%f", &AreaemKm1);
 
 printf("Digite seu PIB:\n");
 scanf("%f", &PIB1);
 
 printf("Digite seus pontos turísticos:\n");
-scanf("%d", &Númerosdepontosturísticos1);
+scanf("%d", &Numerosdepontosturisticos1);
 
 printf("Estado:%s\n", Estado1);
-printf("Código:%s\n", Códigodacarta1);
+printf("Código:%s\n", Codigodacarta1);
 printf("Nome da Cidade:%s\n", Nomedacidade1);
-printf("População:%d\n", População1);
-printf("Área:%f\n", ÁreaemKm²1);
+printf("População:%d\n", Populacao1);
+printf("Área:%f\n", AreaemKm1);
 printf("PIB:%f\n", PIB1);
-printf("Número de Pontos Turísticos:%d\n", Númerosdepontosturísticos1);
+printf("Número de Pontos Turísticos:%d\n", Numerosdepontosturisticos1);
 
 printf("Digite seu estado:\n");
 scanf("%s", Estado2);
 
 printf("Digite seu Código:\n");
-scanf("%s", Códigodacarta2);
+scanf("%s", Codigodacarta2);
 
 printf("Digite sua cidade:\n");
 scanf("%s", Nomedacidade2);
 
 printf("Digite sua população:\n");
-scanf("%d", &População2);
+scanf("%d", &Populacao2);
 
 printf("Digite sua área em Km²:\n");
-scanf("%f", &ÁreaemKm²2);
+scanf("%f", &AreaemKm2);
 
 printf("Digite seu PIB:\n");
 scanf("%f", &PIB2);
 
 printf("Digite seus pontos turísticos:\n");
-scanf("%d", &Númerosdepontosturísticos2);
+scanf("%d", &Numerosdepontosturisticos2);
 
 printf("Estado:%s\n", Estado2);
-printf("Código:%s\n", Códigodacarta2);
+printf("Código:%s\n", Codigodacarta2);
 printf("Nome da Cidade:%s\n", Nomedacidade2);
-printf("População:%d\n",População2);
-printf("Área:%f\n", ÁreaemKm²2);
+printf("População:%d\n",Populacao2);
+printf("Área:%f\n", AreaemKm2);
 printf("PIB:%f\n", PIB2);
-printf("Número de Pontos Turísticos:%d", Númerosdepontosturísticos2);
+printf("Número de Pontos Turísticos:%d", Numerosdepontosturisticos2);
 
 return 0;
 
